4e9f33f180cf4c9581dca2e9ee6c47ad339fa471
M1n01 <minato.cat.love@gmail.com>
2024-02-21 16:25:38 +0900

    Let's start rust

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::env;
+
+fn main() {
+    if env::args().len() > 2 {
+        println!("Usage: {} <config file path>", env::args().nth(0).unwrap());
+    } else {
+        let config_file_path = if env::args().len() == 2 {
+            env::args().nth(1).unwrap()
+        } else {
+            "config/default.json".to_string()
+        };
+        println!("Config file path: {}", config_file_path);
+    }
+}
