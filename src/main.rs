a5d08741d7c10511a202d92179c6ec01d6dc002b
M1n01 <minato.cat.love@gmail.com>
2024-03-03 21:52:50 +0900

    feat: server立ち上げ

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use std::io;
 use std::io::BufReader;
 mod types;
 use crate::types::config::WebServerConfig;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+// mod connection;
+// use crate::connection::event_loop;
 
 fn load_config(file_path: &str) -> Result<WebServerConfig, serde_json::Error>
 {
@@ -20,7 +27,8 @@ fn load_config(file_path: &str) -> Result<WebServerConfig, serde_json::Error>
     }
 }
 
-fn main()
+#[tokio::main]
+async fn main()
 {
     let args: Vec<String> = env::args().collect();
     if args.len() > 2
@@ -35,21 +43,35 @@ fn main()
             _ => "config/default.json",
         };
 
-        let file: Result<File, io::Error> = File::open(config_file_path.to_string());
-        match file
+        match load_config(config_file_path)
         {
-            Ok(f) => match load_config(config_file_path)
+            Ok(config) =>
             {
-                Ok(config) =>
+                let config = Arc::new(config);
+                let addr = format!("{}:{}", config.servers[0].host, config.servers[0].port)
+                    .parse::<SocketAddr>()
+                    .expect("Unable to parse socket address");
+                // サービス関数の定義
+                let make_svc = make_service_fn(move |_conn| {
+                    let config_clone = config.clone(); // クロージャ内で使用するためにクローン
+
+                    // すべてのリクエストに対して同じレスポンスを返す
+                    async move {
+                        Ok::<_, Infallible>(service_fn(move |_req| async move {
+                            Ok::<_, Infallible>(Response::new(Body::from("Hello, World!")))
+                        }))
+                    }
+                });
+                // サーバーの立ち上げ
+                let server = Server::bind(&addr).serve(make_svc);
+
+                println!("Serving on http://{}", addr);
+
+                if let Err(e) = server.await
                 {
-                    println!("Config: {:?}", config);
-                    
+                    eprintln!("server error: {}", e);
                 }
-                Err(e) =>
-                {
-                    println!("Error: {}", e);
-                }
-            },
+            }
             Err(e) =>
             {
                 println!("Error: {}", e);
